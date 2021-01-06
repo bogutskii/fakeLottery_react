55b3cfa6619fcea9e17fb8a8a3e466b6a2777e23
bogutskii <sl9site@gmail.com>
2021-01-06 00:17:58 -0800

    -cheking empty field

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles.css";
 import Inputs from "./Components/Inputs";
 
 export default function App() {
+    const [isEnable, setIsEnable] = useState(true)
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <Inputs/>
+      <Inputs isEnable={isEnable} setIsEnable={(isEnable)=>setIsEnable(isEnable)}/>
     </div>
   );
 }
--- src/Components/Inputs.js
+++ src/Components/Inputs.js
@@ -1,46 +1,54 @@
 import React from "react";
 //import "../src/styles.css";
 
-const Inputs = () => {
-  let playBtn = true;
+const Inputs = (props) => {
+
   const inputNumbers = {
     0: 0,
     1: 0,
     2: 0,
     3: 0,
     4: 0,
-    5: 0
+    5: 0,
+    6: 0
   };
 
   const limit = (e, key) => {
-    if (e.target.value > 69 || e.target.value == "00") {
+    if (e.target.value > 69 || e.target.value === "00") {
       e.target.value = e.target.value.substr(0, 1);
     } else if (e.target.value.length > 2) {
       e.target.value = e.target.value.substr(0, 2);
     }
-    inputNumbers[key] = e.target.value;
+    inputNumbers[key] = +e.target.value;
     checkFields();
-    console.log(e.target.value, key);
+    console.log(e.target.value, key,props.isEnable, inputNumbers);
   };
 
   const checkFields = () => {
-    Object.values(inputNumbers);
+   if(Object.values(inputNumbers).every(el=>el !== 0)){
+       props.setIsEnable(false)
+   }else{
+       props.setIsEnable(true)
+   }
   };
 
   return (
     <div>
-      <input type="number" className="numbox" onKeyUp={(e) => limit(e, 1)} />
-      <input type="number" className="numbox" onKeyUp={(e) => limit(e, 2)} />
-      <input type="number" className="numbox" onKeyUp={(e) => limit(e, 3)} />
-      <input type="number" className="numbox" onKeyUp={(e) => limit(e, 4)} />
-      <input type="number" className="numbox" onKeyUp={(e) => limit(e, 5)} />
-      <input type="number" className="numbox" onKeyUp={(e) => limit(e, 6)} />
-      <input type="number" className="specialNumBox" />
+        <form>
 
-      <button className="btn btn1" disabled={playBtn}>
-        {" "}
-        PLAY{" "}
-      </button>
+      <input type="number" className="numbox" onKeyUp={(e) => limit(e, 0)} required />
+      <input type="number" className="numbox" onKeyUp={(e) => limit(e, 1)} required/>
+      <input type="number" className="numbox" onKeyUp={(e) => limit(e, 2)} required/>
+      <input type="number" className="numbox" onKeyUp={(e) => limit(e, 3)} required/>
+      <input type="number" className="numbox" onKeyUp={(e) => limit(e, 4)} required/>
+      <input type="number" className="numbox" onKeyUp={(e) => limit(e, 5)} required/>
+      <input type="number" className="specialNumBox" onKeyUp={(e) => limit(e, 6)} />
+
+            <button   className="btn btn1" disabled={props.isEnable}  >PLAY</button>
+
+
+        </form>
+
     </div>
   );
 };
